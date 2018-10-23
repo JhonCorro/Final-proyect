400f8e186a18d85b5a4e208032d23e02edd9d21d
JhonCorro <jjcorro@uninorte.edu.co>
2018-10-23 10:43:09 -0500

    Added main to fireb file c:

--- fireb.py
+++ fireb.py
@@ -9,24 +9,25 @@ def pushed():
     print(result)
 
 '''Main Window'''
-window = Tk()
-# Window title
-window.title("RED (Raspberry pi Extracted Data)")
-# Window size
-window.geometry('800x600')
+if __name__ == "__main__":
+    window = Tk()
+    # Window title
+    window.title("RED (Raspberry pi Extracted Data)")
+    # Window size
+    window.geometry('800x600')
 
 
-# Labels
-lbl = Label(window, text="Push Data")
-lb2 = Label(window, text="Get Data")
+    # Labels
+    lbl = Label(window, text="Push Data")
+    lb2 = Label(window, text="Get Data")
 
-# Buttons
-btn = Button(window, text="Do the thing", bg="red", fg="black", command=pushed)
+    # Buttons
+    btn = Button(window, text="Do the thing", bg="red", fg="black", command=pushed)
 
-# Grid
-lbl.grid(column=0, row=0)
-lb2.grid(column=0, row=1)
-btn.grid(column=1, row=0)
+    # Grid
+    lbl.grid(column=0, row=0)
+    lb2.grid(column=0, row=1)
+    btn.grid(column=1, row=0)
 
-# Window loop, must be at the end
-window.mainloop()
+    # Window loop, must be at the end
+    window.mainloop()
